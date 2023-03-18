0f03e9338d06650c13c6ed125a8744979f093ef7
Jname <jiazhigang_888@163.com>
2023-03-18 13:38:17 +0800

    初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'standard-with-typescript',
+        'eslint-config-prettier',
+        'plugin:prettier/recommended'
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 13,
+        sourceType: 'module',
+        ecmaFeatures: {
+            modules: true,
+            jsx: true
+        },
+        requireConfigFile: false,
+        parser: '@typescript-eslint/parser'
+    },
+    plugins: [
+        'vue', // 引入vue的插件 vue <==> eslint-plugin-vue
+        'prettier' // 引入规范插件  prettier <==>  eslint-plugin-prettier
+    ],
+    globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+        withDefaults: 'readonly'
+    },
+    rules: {}
+};
