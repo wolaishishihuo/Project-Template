db4aaa64f355fe1cbb7f74d357032b9959f740b4
jiazhigang <2430250298@qq.com>
2023-04-10 09:27:46 +0800

    update: 更新菜单路由

---
+++ src/views/example/useDynamicForm/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="dynamicForm">
+        <h1>自定义封装 Form表单</h1>
+        <dynamicForm />
+    </div>
+</template>
+
+<script setup lang="ts" name="useDynamicForm">
+import dynamicForm from '@/core/dynamicForm/index.vue';
+</script>
+<style scoped lang="less"></style>
